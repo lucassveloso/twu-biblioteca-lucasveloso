5ccaa459af05d3e9847fcaa69ade0ca756f677a6
lucassveloso <lucas.s.veloso@hotmail.com>
2019-09-25 16:51:08 -0300

    [1.1] Add a welcome message

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,13 +1,33 @@
 package com.twu.biblioteca;
 
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
-public class ExampleTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class BibliotecaTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldPrintTheWelcomeMessage() {
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n";
+        BibliotecaApp.main(new String[] {});
+        assertThat(outContent.toString(), is(expectedMessage));
     }
 }
