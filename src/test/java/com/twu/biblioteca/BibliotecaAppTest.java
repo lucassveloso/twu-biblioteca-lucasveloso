94e3ce49811f34dab76315419a5f8551e0e07909
lucassveloso <lucas.s.veloso@hotmail.com>
2019-09-27 18:19:13 -0300

    [1.4] Configure gradle to add mockito

--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
